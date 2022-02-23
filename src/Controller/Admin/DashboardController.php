6f5ac6fb3e028f838880db9b8d274b380d548e74
petrero <petrero@gmail.com>
2022-02-23 17:56:12 +0200

    2.2 Admin Dashboard
      Securing the Dashboard
      - And because this is just a normal route and controller, it also follows the normal security rules that we would expect. Right now, this means that no security is being applied. I mean, check it out: I'm not even logged in, but I am successfully on the admin dashboard!
    
      So let's secure it! I'll also do this with an attribute. I already have SensioFrameworkExtraBundle installed, so I can say #[IsGranted()] and hit tab to auto-complete that. Let's require any user accessing this controller to have ROLE_ADMIN... that's kind of a base admin role that all admin users have in my app
    
      Now when we refresh... beautiful! We bounced back over to the login page!
    
      To log in, open src/DataFixtures/AppFixtures.php
    
      I have a bunch of dummy users in the database: there's a super admin, a normal admin, and then somebody known as a moderator. We'll talk more about these later when we get deeper into how to secure different parts of your admin for different roles.
    
      Anyways, log in with admin@example.com... password adminpass, and... beautiful! We're back to our dashboard!
    
      Of course, if you want to, instead of using the IsGranted PHP attribute, you could also say $this->denyAccessUnlessGranted(). And you could also go to config/packages/security.yaml and, down at the bottom, add an access_control that protects the entire /admin section
    
      Actually, adding this access_control is basically required: using only the IsGranted attribute is not enough. We'll learn why a bit later.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController {
+	#[IsGranted('ROLE_ADMIN')]
 	#[Route('/admin', name: 'admin')]
 	public function index(): Response {
 		return parent::index();
