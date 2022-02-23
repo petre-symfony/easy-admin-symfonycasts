39df62a14da6799d21543c5f592fb196bff1cc25
petrero <petrero@gmail.com>
2022-02-23 18:39:08 +0200

    3.1 Hello CRUD Controller
      The true reason to use EasyAdmin is for its CRUD controllers. Each CRUD controller will give us a rich set of pages to create, read, update, and delete a single entity. This is where EasyAdmin shines, and the next few minutes are going to be critically important to understand how EasyAdmin works. So, buckle up!
    
      Generating the CRUD Controller
      - We have four entities. Let's generate a CRUD controller for Question first. Find your terminal and run:
    
      symfony console make:admin:crud
      As you can see, it recognizes our four entities. I'll hit 1 for App\Entity\Question, let this generate into the default directory... and with default namespace.
    
      Sweet! This did exactly one thing: it created a new QuestionCrudController.php file. Let's... go open it up!
    
      Linking to the CRUD Controller
      - Cool. But before we look too deeply into this, head over to the admin page and refresh to see... absolutely no difference! We do have a new QuestionCrudController, but these CRUD controllers are totally useless until we link to them from a dashboard. So, back over in DashboardController, down at the bottom... yield MenuItem... but instead of linkToDashboard(), there are a number of other things that we can link to. We want linkToCrud(). Pass this the label - so "Questions" - and some FontAwesome icon classes: fa fa-question-circle. Then, most importantly, pass the entity's class name: Question::class
    
      Behind the scenes, when we click this new link, EasyAdmin will recognize that there is only one CRUD controller for the entity - QuestionCrudController - and will know to use it. And yes, in theory, we can have multiple CRUD controllers for a single entity... and that's something we'll talk about later.
    
      Okay, go refresh to reveal our new link, click and... whoa! This is amazingly cool! We have a slider for the isApproved field, which saves automatically. We also have a search bar on top... and sortable columns to help us find whatever we're looking for.
    
      We can delete, edit... and the form even has a nice calendar widget. This is loaded with rich features out-of-the-box.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Question;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -13,7 +14,7 @@ class DashboardController extends AbstractDashboardController {
 	#[IsGranted('ROLE_ADMIN')]
 	#[Route('/admin', name: 'admin')]
 	public function index(): Response {
-		return parent::index();
+		//return parent::index();
 
 		// Option 1. You can make your dashboard redirect to some common page of your backend
 		//
@@ -30,6 +31,7 @@ class DashboardController extends AbstractDashboardController {
 		// (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
 		//
 		// return $this->render('some/path/my-dashboard.html.twig');
+		return $this->render('admin/index.html.twig');
 	}
 
 	public function configureDashboard(): Dashboard {
@@ -39,6 +41,7 @@ class DashboardController extends AbstractDashboardController {
 
 	public function configureMenuItems(): iterable {
 		yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+		yield MenuItem::linkToCrud('Questions', 'fa fa-question-circle', Question::class);
 		// yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
 	}
 }
