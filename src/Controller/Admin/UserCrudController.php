3e09e71d2416524418d6d2c8dba0ffd8f4584e0b
petrero <petrero@gmail.com>
2022-03-01 21:06:36 +0200

    9.4. Deep Field Configuration
      Hello Field Configurators
      - The answer is something called a Configurator.
    
      Scroll back down to vendor/. I've already opened easycorp/easyadmin-bundle/src/. Earlier, we were looking at the Field/ directory: these are all the built-in fields.
    
      After a field is created, EasyAdmin runs each through a Configurator system that can make additional changes to it. This Configurator/ directory holds those. There are a couple of them - like CommonPreConfigurator - that are applied to every field. It returns true from supports()... and does various normalizations on the field. CommonPostConfigurator is another that applies to every field.
    
      But then, there are also a bunch of configurators that are specific to just one... or maybe a few... field types, including ArrayConfigurator. This configurator does its work when the $field is an ArrayField. The $field->getFieldFqcn() is basically helping to ask:
    
        Hey, is the current field that's being configured an ArrayField? If it is, then call my configure() method so I can do some stuff!
    
      And... yup! Here is where those options are being added. The Configurator system is something we're going to look at more later. Heck we're even going to create our own! For now, just be aware it exists.
    
      Refactoring to ChoiceField
      - So, hmm. In our situation, we don't want the ArrayConfigurator to do its work. But, unfortunately, we don't really have a choice! The Configurator is always going to apply its logic if we're dealing with an ArrayField.
    
      And actually, that's fine! Back in UserCrudController.php, I didn't realize it at first, but there's also a ChoiceField!
    
      Hold Cmd or Ctrl to open it. Yup, we can see that it already uses ChoiceType. So, we don't need to take ArrayField and try to turn it into a choice... there's already a built-in ChoiceField made for this!
    
      And now we don't need to set the form type... and we don't need the help or the form type options. I probably could set the choices that way, but the ChoiceField has a special method called ->setChoices(). Pass that same thing: array_combine($roles, $roles). For the other options, we can say ->allowMultipleChoices() and ->renderExpanded().
    
      How nice is that?
    
      Let's try this thing. Refresh and... that is what I was hoping for! Back on the index... ChoiceType still renders as a nice comma-separated list.
    
      Oh, and by the way: if you want to see the logic that makes ChoiceType render as a comma-separated list, there a ChoiceConfigurator.php. If you open that... and scroll to the bottom - beyond a lot of normalization code - here it is: $field->setFormattedValue() where it implodes the $selectedChoices with a comma.
    
      Rendering ChoiceList as Badges
      - Oh, and speaking of this type - let me close some core classes - one other method we can call is ->renderAsBadges()
    
      That affects the "formatted value" that we just saw... and turns it into these little guys. Cute!
    
      Next, let's handle our user's  field, which needs to be an upload field!

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
@@ -34,11 +35,10 @@ class UserCrudController extends AbstractCrudController {
 		yield DateField::new('createdAt')
 			->hideOnForm();
 		$roles = ['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_USER'];
-		yield ArrayField::new('roles')
-			->setHelp('Available roles: ROLE_SUPER_ADMIN, ROLE_ADMIN, ROLE_MODERATOR, ROLE_USER')
-			->setFormType(ChoiceType::class)
-			->setFormTypeOptions([
-				'choices' => array_combine($roles, $roles)
-			]);
+		yield ChoiceField::new('roles')
+			->setChoices(array_combine($roles, $roles))
+			->allowMultipleChoices()
+			->renderExpanded()
+			->renderAsBadges();
 	}
 }
