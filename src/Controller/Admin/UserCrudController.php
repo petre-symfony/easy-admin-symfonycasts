0b0430160ac1fbefe37faf105ef5aa13df6c42ee
petrero <petrero@gmail.com>
2022-03-01 19:39:31 +0200

    7.2. Configuring Fields
      Field Types
      - So EasyAdmin has many different types of fields, like text fields, boolean fields, and association fields... and it does its best to guess which type to use. In this case, you can't really see it, but when we said id, it guessed that this is an IdField. Instead of just saying Field::new() and letting it guess, I often prefer being explicit: IdField::new()
    
      Watch: when we refresh... that makes absolutely no difference! It was already guessing that this was an IdField.
    
      Cool! So how do we figure out what all of the field types are? Documentation is the most obvious way. If you look on the web debug toolbar, there's a little EasyAdmin icon. Click into that... to see some basic info about the page... with a handy link to the documentation. Open that up. It has a "Field Types" section down a ways. Yup, there's your big list of all the different field types inside of EasyAdmin.
    
      Or, if you want to go rogue, you find this directly in the source code. Check out vendor/easycorp/easyadmin-bundle/src/Field. Here is the directory that holds all the different possible field types.
    
      Back in our CRUD controller, let's add a few more fields.
    
      If you look in the User entity, you can see $id, $email, $roles, $password, $enabled, $firstName, $lastName, $avatar... and then a couple of association fields:
    
      283 lines  src/Entity/User.php
      class User implements UserInterface, PasswordAuthenticatedUserInterface
      {
        use TimestampableEntity;
        #[ORM\Id]
        #[ORM\GeneratedValue]
        #[ORM\Column]
        private ?int $id;
        #[ORM\Column(length: 180, unique: true)]
        private ?string $email;
        #[ORM\Column(type: Types::JSON)]
        private array $roles = [];
        /**
         * The hashed password
         */
        #[ORM\Column]
        private ?string $password;
        /**
         * The plain non-persisted password
         */
        private ?string $plainPassword;
        #[ORM\Column]
        private bool $enabled = true;
        #[ORM\Column]
        private ?string $firstName;
        #[ORM\Column]
        private ?string $lastName;
        #[ORM\Column(nullable: true)]
        private ?string $avatar;
        #[ORM\OneToMany('askedBy', Question::class)]
        private Collection $questions;
        #[ORM\OneToMany('answeredBy', Answer::class)]
        private Collection $answers;
      }
      We won't need to manage all of these in the admin, but we will want most of them.
    
      Add yield TextField::new('firstName')... repeat that for $lastName... and then for the $enabled field, let's yield BooleanField::new('enabled'). We also have a $createdAt field... so yield DateField::new('createdAt')
    
      So I'm just listing the same properties that we see in the entity. Well, we don't see $createdAt... but that's only because it lives inside of the TimestampableEntity trait
    
      Anyways, with just this config, if we move over and refresh... beautiful! The text fields render normal text, the DateField knows how to print dates and the BooleanField gives us this nice little switch!

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController {
 	public static function getEntityFqcn(): string {
@@ -13,6 +16,10 @@ class UserCrudController extends AbstractCrudController {
 
 
 	public function configureFields(string $pageName): iterable {
-		yield Field::new('id');
+		yield IdField::new('id');
+		yield TextField::new('firstName');
+		yield TextField::new('lastName');
+		yield BooleanField::new('enabled');
+		yield DateField::new('createdAt');
 	}
 }
