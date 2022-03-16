804cc0db2b671a026b6f04567daa5bc29e9437a3
petrero <petrero@gmail.com>
2022-03-16 19:50:12 +0200

    12.1 The AssociationField
      Let's configure the fields for some of our other CRUD controllers. Go to the "Questions" page. This shows the default field list. We can do better. Open QuestionCrudController, uncomment configureFields(), and then... let's yield some fields! I'm going to write that down in my poetry notebook.
    
      Let's yield a field for IdField... and call ->onlyOnIndex(). Then yield Field::new('name')
    
      Yea, yea... I'm being lazy. I'm using Field::new() and letting it guess the field type for me. This should be good enough most of the time, unless you need to configure something specific to a field type.
    
      Copy that... and paste this two more times for votes and createdAt. For createdAt, don't forget to add ->hideOnForm()
    
      Cool! Find your browser, refresh and... good start!

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -4,20 +4,21 @@ namespace App\Controller\Admin;
 
 use App\Entity\Question;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class QuestionCrudController extends AbstractCrudController {
 	public static function getEntityFqcn(): string {
 		return Question::class;
 	}
 
-	/*
-	public function configureFields(string $pageName): iterable
-	{
-			return [
-					IdField::new('id'),
-					TextField::new('title'),
-					TextEditorField::new('description'),
-			];
+
+	public function configureFields(string $pageName): iterable {
+		yield IdField::new('id')
+			->onlyOnIndex();
+		yield Field::new('name');
+		yield Field::new('votes');
+		yield Field::new('createdAt')
+			->hideOnForm();
 	}
-	*/
 }
