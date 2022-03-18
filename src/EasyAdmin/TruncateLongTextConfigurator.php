9048c91967e947f7269b466092dea2d75d1be5c4
petrero <petrero@gmail.com>
2022-03-18 17:26:04 +0200

    16.1. Field Configurator Logic
      We just bootstrapped a field configurator: a super-hero-like class where we get to modify any field in any CRUD section from the comfort of our home. We really do live in the future.
    
      At this point in the process, what EasyAdmin gives us is something called a FieldDto, which, as you can see, contains all the info about this field, like its value, formatted value, form type, template path and much more.
    
      FieldDto vs Field
      - One thing you might have noticed is that this is a FieldDto. But when we're in our CRUD controllers, we're dealing with the Field class. Interesting. This is a pattern that EasyAdmin follows a lot. When we're configuring things, we use an easy class like Field... where Field gives us a lot of nice methods to control everything about it.
    
      But behind the curtain, the entire purpose of the Field class - or any of the other field classes - is to take all of the info we give it and create a FieldDto. I'll call ->formatValue() temporarily and hold Cmd or Ctrl to jump into that. This moved us into a FieldTrait that Field uses.
    
      And check it out! When we call formatValue(), what that really does is say $this->dto->setFormatValueCallable(). That Dto is the FieldDto. So we call nice methods on Field, but in the background, it uses all of that info to craft this FieldDto. This means that the FieldDto contains the same info as the Field objects, but its data, structure and methods are all a bit different.
    
      Truncating the Formatted Value
      - Ok: back to our goal of truncating long textarea fields. Add a private const MAX_LENGTH = 25 to keep track of our limit
    
      Then, below, if (strlen($field->getFormattedValue())) is less than or equal to self::MAX_LENGTH, then just return
    
      And yes, I totally forgot about the <= self::MAX_LENGTH part. I'll add that later. You should add it now.
    
      Anyways, assuming you wrote this correctly, it says that if the formatted value is already less than 25 characters, don't bother changing it: just let EasyAdmin render like normal.
    
      Below, let's truncate: $truncatedValue =... and I'll use the u() function. Hit Tab to autocomplete that. Just like with a class, it added a use statement on top
    
      The u function gives us a UnicodeString object from Symfony's String component.
    
      Pass this $field->getFormattedValue() and call ->truncate() with self::MAX_LENGTH, ... and false
    
      The last argument just makes truncate a little cleaner. Oh, and I forgot a colon right there. That's better. Finally, call $field->setFormattedValue() and pass it $truncatedValue to override what the formatted value would be
    
      Let's try it! Move over, refresh and... absolutely nothing happens! All of the items in this column still have the same length as before. What's happening? It's not the bug in my code... something else is going on. But what?

--- src/EasyAdmin/TruncateLongTextConfigurator.php
+++ src/EasyAdmin/TruncateLongTextConfigurator.php
@@ -7,14 +7,23 @@ use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldConfiguratorInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use function Symfony\Component\String\u;
 
 class TruncateLongTextConfigurator implements FieldConfiguratorInterface {
+	private const MAX_LENGTH = 25;
+
 	public function supports(FieldDto $field, EntityDto $entityDto): bool {
 		return $field->getFieldFqcn() === TextareaField::class;
 	}
 
 	public function configure(FieldDto $field, EntityDto $entityDto, AdminContext $context): void {
-		dd($field);
+		if (strlen($field->getFormattedValue()) <= self::MAX_LENGTH) {
+			return;
+		};
+
+		$truncatedValue = u($field->getFormattedValue())
+			->truncate(self::MAX_LENGTH, '...', false);
+		$field->setFormattedValue($truncatedValue);
 	}
 
 }
