9e150325aeb38b109ea013a2734a79d73d6e4554
petrero <petrero@gmail.com>
2022-03-19 21:30:28 +0200

    28.2. Security Voter & Entity Permissions
      Granting Access to ROLE_SUPER_ADMIN
      - This is great! Except that super admins should be able to see all users. Right now, no matter who I log in as, we're only going to show my user. To solve this, down in our logic, we can check to see if the user has ROLE_SUPER_ADMIN. But to do that, we need a service.
    
      Add public function __construct(), and inject the Security service from Symfony (I'll call it ). Hit "alt" + "enter", and go to "Initialize properties" to create that property and set it. Then, down here, return true if $user === $subject or if $this->security->isGranted('ROLE_SUPER_ADMIN')
    
      Cool! I won't bother logging in as a super admin to try this. But if we did, we would now see every user.

--- src/Security/Voter/AdminUserVoter.php
+++ src/Security/Voter/AdminUserVoter.php
@@ -5,9 +5,17 @@ namespace App\Security\Voter;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class AdminUserVoter extends Voter {
+	private Security $security;
+
+	public function __construct(Security $security) {
+
+		$this->security = $security;
+	}
+
 	protected function supports(string $attribute, $subject): bool {
 		// replace with your own logic
 		// https://symfony.com/doc/current/security/voters.html
@@ -29,7 +37,7 @@ class AdminUserVoter extends Voter {
 		// ... (check conditions and return true to grant permission) ...
 		switch ($attribute) {
 			case 'ADMIN_USER_EDIT':
-				return $user === $subject;
+				return $user === $subject || $this->security->isGranted('ROLE_SUPER_ADMIN');
 		}
 
 		return false;
