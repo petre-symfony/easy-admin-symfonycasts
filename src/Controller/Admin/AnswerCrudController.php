8bacb4d0455daa624cf817c7e521ecbcf2bd4825
petrero <petrero@gmail.com>
2022-03-20 15:53:00 +0200

    30.5. Multiple Cruds for a Single Entity?
      To fix this, once again, we just need to be explicit. Add ->setCrudController(QuestionCrudController::class).
    
      This time, I'll refresh... go down to the Question field, search for the string and... it finds it!
    
      Next, what if we want to run some code before or after an entity is updated, created, or deleted? EasyAdmin has two solutions: Events and controller methods.

--- src/Controller/Admin/AnswerCrudController.php
+++ src/Controller/Admin/AnswerCrudController.php
@@ -22,6 +22,7 @@ class AnswerCrudController extends AbstractCrudController {
 		yield VotesField::new('votes');
 		yield AssociationField::new('question')
 			->autocomplete()
+			->setCrudController(QuestionCrudController::class)
 			->hideOnIndex();
 		yield AssociationField::new('answeredBy');
 		yield Field::new('createdAt')
